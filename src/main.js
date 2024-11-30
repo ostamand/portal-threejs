e14ea4c474f82a2e517e01a8f43335649c17566a
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2024-11-30 14:44:13 -0500

    Add model

--- src/main.js
+++ src/main.js
@@ -1,24 +1,86 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const canvas = document.getElementById("root");
 
-setupCounter(document.querySelector('#counter'))
+// window sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+window.addEventListener("resize", () => {
+    // update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+    50,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+camera.position.z = 5;
+camera.position.y = 3;
+camera.position.x = 2;
+
+// orbit controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// loaders
+const gltfLoader = new GLTFLoader();
+const textureLoader = new THREE.TextureLoader();
+
+// textures
+const bakedTexture = textureLoader.load("models/baked.jpg");
+bakedTexture.flipY = false;
+bakedTexture.colorSpace = THREE.SRGBColorSpace;
+
+// materials
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture });
+const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xf3e163 });
+
+// load environment
+gltfLoader.load("models/portal_baked.glb", (loadedAsset) => {
+    loadedAsset.scene.traverse((child) => {
+        child.material = bakedMaterial;
+    });
+    scene.add(loadedAsset.scene);
+});
+
+// loop
+const clock = new THREE.Clock();
+let lastElapsedTime = 0;
+
+const loop = () => {
+    // delta time
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - lastElapsedTime;
+    lastElapsedTime = elapsedTime;
+
+    renderer.render(scene, camera);
+    controls.update(deltaTime);
+    window.requestAnimationFrame(loop);
+};
+
+loop();
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
 export default {
-  base: "/portal-threejs/",
+    base: "/portal-threejs/",
+    publicDir: "./static/",
 };
